cc9690980c1054d05776fc0355749dcaba4c3810
Antonin-BygonBlod <antonin.hunault@yahoo.fr>
2022-04-13 18:49:23 +0200

    utils

---
+++ src/Utils/Pair.java
@@ -0,0 +1,28 @@
+package Utils;
+
+public class Pair<T1, T2> {
+	private String key;
+	private String value;
+
+	public Pair(String key, String value) {
+		this.setKey(key);
+		this.setValue(value);
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
