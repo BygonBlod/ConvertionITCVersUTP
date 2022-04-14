d79dce5d57ba45b1f18512d44c0c585f7981a5fc
Antonin-BygonBlod <antonin.hunault@yahoo.fr>
2022-04-14 11:48:55 +0200

    convertionrooms de USP vers ITC

--- src/main/MainVersITC.java
+++ src/main/MainVersITC.java
@@ -9,7 +9,6 @@ import USP.ReadWrite.ReadUSP;
 public class MainVersITC {
 	public static void main(String[] args) {
 		Timetabling time = ReadUSP.getTimeTabling(args[0]);
-		// WriteUSP.write(time, "fileUSP.xml");
 
 		ProblemITC problem = ConvertisseurITC.getProblem(time);
 		WriteITC.write(problem, "testUSPversITC.xml");
