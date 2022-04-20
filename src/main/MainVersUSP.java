d48fcc86cf29aa7ebf4c48faf5494ce9254cac6b
Antonin-BygonBlod <antonin.hunault@yahoo.fr>
2022-04-20 18:07:26 +0200

    suppression des commentaires de la convertion et de l'écriture

--- src/main/MainVersUSP.java
+++ src/main/MainVersUSP.java
@@ -1,19 +1,23 @@
 package main;
 
+import Convertion.VersUSP.ConvertisseurUSP;
 import ITC.Model.ProblemITC;
 import ITC.ReadWrite.ReadITC;
+import ITC.ReadWrite.WriteITC;
+import USP.Model.Timetabling;
+import USP.ReadWrite.WriteUSP;
 
 public class MainVersUSP {
 
 	public static void main(String[] args) {
-		ProblemITC problem = ReadITC.getProblem(args[0]);
-		/*
-		 * WriteITC.write(problem, "testApresITC.xml");
-		 * 
-		 * Timetabling time = ConvertisseurUSP.getTimeTabling(problem);
-		 * 
-		 * WriteUSP.write(time, "testITCversUSP.xml");
-		 */
+		ProblemITC problem = ReadITC.getProblem(args[0], false);
+
+		WriteITC.write(problem, "testApresITC.xml");
+
+		Timetabling time = ConvertisseurUSP.getTimeTabling(problem);
+
+		WriteUSP.write(time, "testITCversUSP.xml");
+
 	}
 
 }
